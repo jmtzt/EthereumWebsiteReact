bd7b085b3def1f2d4c7ebe74488fcc841cee2b4b
Adilson <adilsonfrankneia@gmail.com>
2021-03-26 17:16:57 -0300

    initial commit

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import styles from './styles.module.css';
+import Button from '@material-ui/core/Button';
+import { TextField } from "@material-ui/core";
 
 function Login() {
   return (
-    <div>
-      <p>Login page</p>
+    <div className={styles.container}>
+        <div className={styles.content}> 
+            <div className={styles.item1}>
+                logo
+                
+            </div>
+
+            <div className={styles.inputs}>
+                <TextField id="standard-basic" label="Login"/>
+                <div className={styles.password}>
+                  <TextField id="standard-basic" label="Senha" style={{width:'100%'}}/>
+                </div>
+            </div>
+
+            <div className={styles.button}>
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
+            </div>
+        </div>
     </div>
   );
 }
